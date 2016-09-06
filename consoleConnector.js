da73481bdb75de705bdd6298726ce2898a3ab726
Burak Donbay <burak.donbay@gmail.com>
2016-09-06 21:34:33 +0300

    Console Connector,privacy and termofuse pages added

--- consoleConnector.js
+++ consoleConnector.js
@@ -1,36 +1,11 @@
 //=========================================================
-// Chat Connector
+// Console Connector
 //=========================================================
 
-var restify = require('restify');
 var builder = require('botbuilder');
 
-//=========================================================
-// Bot Setup
-//=========================================================
-
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
-});
-
-// Environment variables
-var envs = require('envs');
-const env = require('env2')('./config.env');
-
-// Create chat bot
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-
+var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
-
-//=========================================================
-// Bots Dialogs
-//=========================================================
 
 var intents = new builder.IntentDialog();
 bot.dialog('/', intents);
