54a67eddb7d9eb660c1344364f60eb1d7c399c98
Burak Donbay <burak.donbay@gmail.com>
2016-09-06 22:48:30 +0300

    LUIS cortana-English added

---
+++ Examples/3_LUIS_connector_basic.js
@@ -0,0 +1,16 @@
+var builder = require('botbuilder');
+
+// Create bot and bind to console
+var connector = new builder.ConsoleConnector().listen();
+var bot = new builder.UniversalBot(connector);
+
+// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
+var model = 'https://api.projectoxford.ai/luis/v1/application?id=c413b2ef-382c-45bd-8ff0-f76d60e2a821&subscription-key=fa5d0e8c6499450bb0f4f45f8ef1ff84&q=';
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+bot.dialog('/', dialog);
+
+// Add intent handlers
+dialog.matches('builtin.intent.alarm.set_alarm', builder.DialogAction.send('Creating Alarm'));
+dialog.matches('builtin.intent.alarm.delete_alarm', builder.DialogAction.send('Deleting Alarm'));
+dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I can only create & delete alarms."));
